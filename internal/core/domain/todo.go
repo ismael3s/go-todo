e5ff70824e678193b9dd58afdc6cdf196c331a1a
Ismael Santana <souz4ismael@gmail.com>
2022-08-13 11:03:37 -0300

    feat: update domain todo and their ports

--- internal/core/domain/todo.go
+++ internal/core/domain/todo.go
@@ -1,13 +1,13 @@
 package domain
 
 type Todo struct {
-	ID   string `json:"id"`
-	Name string `json:"name"`
+	ID    string `json:"id"`
+	Title string `json:"title"`
 }
 
-func NewTodo(id string, name string) *Todo {
+func NewTodo(id string, title string) *Todo {
 	return &Todo{
-		ID:   id,
-		Name: name,
+		ID:    id,
+		Title: title,
 	}
 }
--- internal/core/ports/todo.go
+++ internal/core/ports/todo.go
@@ -4,8 +4,10 @@ import "github.com/ismael3s/go-todo/internal/core/domain"
 
 type TodoService interface {
 	GetTodos() ([]*domain.Todo, error)
+	GetTodo(id string) (*domain.Todo, error)
 }
 
 type TodoRepository interface {
 	GetTodos() ([]*domain.Todo, error)
+	GetTodo(id string) (*domain.Todo, error)
 }
