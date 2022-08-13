c8dcfbc324557618a661b0499f5bfe447a5d70f3
Ismael Santana <souz4ismael@gmail.com>
2022-08-12 23:46:12 -0300

    feat: added failed tests

--- internal/infra/persistence/todo/in_memory_todo_repository.go
+++ internal/infra/persistence/todo/in_memory_todo_repository.go
@@ -4,14 +4,20 @@ import "github.com/ismael3s/go-todo/internal/core/domain"
 
 type InMemoryTodoRepository struct {
 	todos []*domain.Todo
+	err   error
 }
 
-func NewInMemoryTodoRepository() *InMemoryTodoRepository {
+func NewInMemoryTodoRepository(err error) *InMemoryTodoRepository {
 	return &InMemoryTodoRepository{
 		todos: []*domain.Todo{},
+		err:   err,
 	}
 }
 
 func (r *InMemoryTodoRepository) GetTodos() ([]*domain.Todo, error) {
+	if r.err != nil {
+		return nil, r.err
+	}
+
 	return r.todos, nil
 }
