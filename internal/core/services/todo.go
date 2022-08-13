4708e37c29c96fbd640fecdf8df5369382142811
Ismael Santana <souz4ismael@gmail.com>
2022-08-13 11:03:56 -0300

    feat: impl the new todo service and the in memory repo

--- internal/core/services/todo.go
+++ internal/core/services/todo.go
@@ -5,10 +5,22 @@ import (
 	"github.com/ismael3s/go-todo/internal/core/ports"
 )
 
+// s *TodoService ports.TodoService
+
 type TodoService struct {
 	todoRepository ports.TodoRepository
 }
 
 func (s *TodoService) GetTodos() ([]*domain.Todo, error) {
 	return s.todoRepository.GetTodos()
+}
+
+func (s *TodoService) GetTodo(id string) (*domain.Todo, error) {
+	todo, err := s.todoRepository.GetTodo(id)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return todo, nil
 }
--- internal/infra/persistence/todo/in_memory_todo_repository.go
+++ internal/infra/persistence/todo/in_memory_todo_repository.go
@@ -1,15 +1,19 @@
 package todo
 
-import "github.com/ismael3s/go-todo/internal/core/domain"
+import (
+	"errors"
+
+	"github.com/ismael3s/go-todo/internal/core/domain"
+)
 
 type InMemoryTodoRepository struct {
 	todos []*domain.Todo
 	err   error
 }
 
-func NewInMemoryTodoRepository(err error) *InMemoryTodoRepository {
+func NewInMemoryTodoRepository(todos []*domain.Todo, err error) *InMemoryTodoRepository {
 	return &InMemoryTodoRepository{
-		todos: []*domain.Todo{},
+		todos: todos,
 		err:   err,
 	}
 }
@@ -20,4 +24,18 @@ func (r *InMemoryTodoRepository) GetTodos() ([]*domain.Todo, error) {
 	}
 
 	return r.todos, nil
+}
+
+func (r *InMemoryTodoRepository) GetTodo(id string) (*domain.Todo, error) {
+	if r.err != nil {
+		return nil, r.err
+	}
+
+	for _, todo := range r.todos {
+		if todo.ID == id {
+			return todo, nil
+		}
+	}
+
+	return nil, errors.New("todo not found")
 }
